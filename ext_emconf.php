fbd46e51d6254a9f154e99d77570a967ed80362c
Karsten <captnnowi@gmx.de>
2017-06-03 23:03:03 +0200

    FIX: clean up dependencies, set version number to 2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'title' => 'Operations',
 	'description' => 'Manage (firefighter,Feuerwehr) operations with detailed reports, images used resources or vehicles, google map view for locations, slider and lightbox for images. Possibly to use it for other reportable events like THW and so on.',
 	'category' => 'plugin',
-	'version' => '1.3.6',
+	'version' => '2.0.0',
 	'state' => 'beta',
 	'uploadfolder' => true,
 	'createDirs' => '',
@@ -26,9 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' =>
 		array (
-			'php' => '5.5',
-			'extbase' => '6.2',
-			'fluid' => '6.2',
+			'php' => '5.6',
 			'typo3' => '8.7 - 8.7.99',
 		),
 		'conflicts' =>
