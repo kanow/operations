95c409b0aed805bfc7b519d4021a3021f91ae420
Karsten <captnnowi@gmx.de>
2017-06-05 20:02:10 +0200

    FIX: clean up code

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "operations".
- *
- * Auto generated 31-07-2014 23:32
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = array (
 	'title' => 'Operations',
 	'description' => 'Manage (firefighter,Feuerwehr) operations with detailed reports, images used resources or vehicles, google map view for locations, slider and lightbox for images. Possibly to use it for other reportable events like THW and so on.',
