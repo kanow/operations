b60187e85047367556e86456ed9b334f6c6c409e
Karsten <captnnowi@gmx.de>
2017-10-23 22:15:44 +0200

    FIX: comment the unnecessary methods groupedByYear's

--- Classes/Controller/OperationController.php
+++ Classes/Controller/OperationController.php
@@ -171,7 +171,7 @@ class OperationController extends BaseController
         $years = $this->generateYears();
         $types = $this->typeRepository->findAll()->toArray();
 
-        $operationsGroupedByYear = $this->operationRepository->countGroupedByYear();
+//        $operationsGroupedByYear = $this->operationRepository->countGroupedByYear();
         $operationsGroupedByYearAndType = $this->operationRepository->countGroupedByYearAndType($years,$types);
 
         $this->view->assignMultiple(
