28216537c691539889b2165278960b2ba0afb511
Karsten Nowak <captnnowi@gmx.de>
2019-03-04 22:15:56 +0100

    [BUGFIX] use leaflet also in single view map

--- Resources/Public/Js/map/leaflet-map.js
+++ Resources/Public/Js/map/leaflet-map.js
@@ -1,17 +1,18 @@
 // See post: http://asmaloney.com/2014/01/code/creating-an-interactive-map-with-leaflet-and-openstreetmap/
 
+var mapData = jQuery('.leaflet-map').data('map');
+console.log(mapData.zoomControl);
 var map = L.map( 'leaflet-map', {
     // center and zoom ahs to be set manually if no fitBounds is activated
     // center: [50.0, 11.0],
     // zoom: 8
+    zoomControl: mapData.zoomControl
 });
 
 L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   subdomains: ['a', 'b', 'c']
 }).addTo( map )
-
-var mapData = jQuery('.leaflet-map').data('map');
 
 var myIcon = L.icon({
   iconUrl: mapData.mapFolder + 'images/operations-pin24.png',
@@ -25,9 +26,15 @@ var myIcon = L.icon({
 
 // get markers(items) from each item and push it to an array
 var markers = [];
-jQuery('.list-item').each(function() {
-    markers.push(jQuery(this).data('marker'))
-});
+if(jQuery('.leaflet-map.single').length > 0)
+{
+    markers.push(jQuery('.leaflet-map.single').data('marker'));
+} else {
+    jQuery('.list-item').each(function() {
+        markers.push(jQuery(this).data('marker'))
+    });
+}
+
 
 // automatically zoom and centering depending of markers
 var arrayOfLatLngs = [];
@@ -38,13 +45,27 @@ for (var i=0; i < markers.length; ++i)
 var bounds = new L.LatLngBounds(arrayOfLatLngs);
 map.fitBounds(bounds);
 
-// set content for popup
-for ( var i=0; i < markers.length; ++i )
+
+if(jQuery('.leaflet-map.single').length > 0)
 {
-  markerUri = markers[i].uri;
-  markerTitle = markers[i].title;
-  markerTeaser = markers[i].teaser;
- L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
-  .bindPopup( '<a href="' + markerUri + '">' + markerTitle + '</a>' + '<p>' + markerTeaser + '</p>'  )
-  .addTo( map );
+    // set content for popup
+    for ( var i=0; i < markers.length; ++i )
+    {
+        markerTitle = markers[i].title;
+        L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
+            .bindPopup( '<p>' + markerTitle + '</p>'  )
+            .addTo( map );
+    }
+} else {
+    // set content for popup
+    for ( var i=0; i < markers.length; ++i )
+    {
+        markerUri = markers[i].uri;
+        markerTitle = markers[i].title;
+        markerTeaser = markers[i].teaser;
+        L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
+            .bindPopup( '<a href="' + markerUri + '">' + markerTitle + '</a>' + '<p>' + markerTeaser + '</p>'  )
+            .addTo( map );
+    }
 }
+
