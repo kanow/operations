c6177ef0ea0e94f93a3c54af31a3058720984731
Karsten Nowak <captnnowi@gmx.de>
2019-03-08 08:35:55 +0100

    [TASK] improve and cleanup code in ext_tables

--- ext_tables.php
+++ ext_tables.php
@@ -4,78 +4,30 @@ if (!defined('TYPO3_MODE')) {
 }
 use TYPO3\CMS\Core\Utility\GeneralUtility as GeneralCoreUtility;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	'KN.'.$_EXTKEY,
-	'List',
-	'Operations'
-);
+$icons = [
+    'wizard-icon' => 'tx_operations_operation.svg',
+    'operation' => 'tx_operations_operation.svg',
+    'assistance' => 'tx_operations_assistance.svg',
+    'resource' => 'tx_operations_resource.svg',
+    'vehicle' => 'tx_operations_vehicle.svg',
+    'type' => 'tx_operations_type.svg'
+];
 
-$extensionName = strtolower(GeneralCoreUtility::underscoredToUpperCamelCase($_EXTKEY));
-$pluginName = strtolower('List');
-$pluginSignature = $extensionName.'_'.$pluginName;
-
-// get first main part of TYPO3 version number
-$currentTypo3Version = \KN\Operations\Utility\Div::getPartOfTypo3Version();
-
-
-// @todo use array and foreach for cycle
 $iconRegistry = GeneralCoreUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-// register wizard icon for operation
-$iconRegistry->registerIcon(
-    'ext-operations-wizard-icon',
-    BitmapIconProvider::class,
-    ['source' => 'EXT:operations/Resources/Public/Icons/tx_operations_operation.svg']
-);
-// register icon for operation
-$iconRegistry->registerIcon(
-     'ext-operations-operation',
-     BitmapIconProvider::class,
-     ['source' => 'EXT:operations/Resources/Public/Icons/tx_operations_operation.svg']
-);
-// register icon for assistance
-$iconRegistry->registerIcon(
-     'ext-operations-assistance',
-     BitmapIconProvider::class,
-     ['source' => 'EXT:operations/Resources/Public/Icons/tx_operations_assistance.svg']
-);
-// register icon for resource
-$iconRegistry->registerIcon(
-     'ext-operations-resource',
-     BitmapIconProvider::class,
-     ['source' => 'EXT:operations/Resources/Public/Icons/tx_operations_resource.svg']
-);
-// register icon for vehicle
-$iconRegistry->registerIcon(
-     'ext-operations-vehicle',
-     BitmapIconProvider::class,
-     ['source' => 'EXT:operations/Resources/Public/Icons/tx_operations_vehicle.svg']
-);
-// register icon for type
-$iconRegistry->registerIcon(
-     'ext-operations-type',
-     BitmapIconProvider::class,
-     ['source' => 'EXT:operations/Resources/Public/Icons/tx_operations_type.svg']
-);
+foreach ($icons as $key => $value)
+{
+    $iconRegistry->registerIcon(
+        'ext-operations-'.$key,
+        SvgIconProvider::class,
+        ['source' => 'EXT:operations/Resources/Public/Icons/'.$value]
+    );
+}
 
-
-
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key,pages';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/flexform_list.xml');
-
-ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Operations');
-
-ExtensionManagementUtility::addLLrefForTCAdescr('tx_operations_domain_model_operation', 'EXT:operations/Resources/Private/Language/locallang_csh_tx_operations_domain_model_operation.xlf');
-
-ExtensionManagementUtility::addLLrefForTCAdescr('tx_operations_domain_model_assistance', 'EXT:operations/Resources/Private/Language/locallang_csh_tx_operations_domain_model_assistance.xlf');
-
-ExtensionManagementUtility::addLLrefForTCAdescr('tx_operations_domain_model_vehicle', 'EXT:operations/Resources/Private/Language/locallang_csh_tx_operations_domain_model_vehicle.xlf');
-
-ExtensionManagementUtility::addLLrefForTCAdescr('tx_operations_domain_model_resource', 'EXT:operations/Resources/Private/Language/locallang_csh_tx_operations_domain_model_resource.xlf');
-
-ExtensionManagementUtility::addLLrefForTCAdescr('tx_operations_domain_model_type', 'EXT:operations/Resources/Private/Language/locallang_csh_tx_operations_domain_model_type.xlf');
-
-$TCA['sys_file_reference']['columns']['uid_local']['config']['foreign_table'] = 'sys_file';
+$llRefs = ['operation','assistance','vehicle','resource','type'];
+foreach ($llRefs as $llRef)
+{
+    ExtensionManagementUtility::addLLrefForTCAdescr('tx_operations_domain_model_'.$llRef, 'EXT:operations/Resources/Private/Language/locallang_csh_tx_operations_domain_model_'.$llRef.'.xlf');
+}
 
