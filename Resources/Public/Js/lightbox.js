774e647040ca4b270f3bc3797699c8b23aff547c
Karsten Nowak <captnnowi@gmx.de>
2019-03-08 23:01:07 +0100

    [!!! TASK] remove unnecessary js and css include there settings

--- Resources/Public/Js/lightbox.js
+++
@@ -1,8 +0,0 @@
-	// magnific Popup a jquery Lightbox plugin
-	$('.single .images .slides').magnificPopup({
-		delegate: 'li:not(".clone") a',
-		type: 'image',
-		gallery: {
-			enabled: true
-		}  
-	});
