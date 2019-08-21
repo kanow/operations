7900fac577e02044b765c78f6269560c94ea1a36
Karsten Nowak <captnnowi@gmx.de>
2019-08-21 21:21:20 +0200

    [BUGFIX] remove duplicated class import

--- Classes/Domain/Repository/OperationRepository.php
+++ Classes/Domain/Repository/OperationRepository.php
@@ -34,7 +34,6 @@ use TYPO3\CMS\Extbase\Persistence\Repository;
  ***************************************************************/
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  *
