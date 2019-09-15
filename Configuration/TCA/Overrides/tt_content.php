79618b4e9da3a7d14c4b51a5366810a16c7337a0
Karsten Nowak <captnnowi@gmx.de>
2019-09-15 22:16:11 +0200

    [TASK] Improve code for plugin configuration and registration

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,35 +3,38 @@ if (!defined ('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-use TYPO3\CMS\Core\Utility\GeneralUtility as GeneralCoreUtility;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
-$extensionName = strtolower(GeneralCoreUtility::underscoredToUpperCamelCase('operations'));
-$pluginName = strtolower('List');
-$pluginSignature = $extensionName.'_'.$pluginName;
+$pluginConfiguration = [
+    '0' => [
+        'pluginName' => 'List',
+        'extensionName' => 'operations',
+        'flexformFile' => 'FILE:EXT:operations/Configuration/FlexForms/flexform_list.xml',
+        'title' => 'Operations',
+        'icon' => 'ext-operations-wizard-icon'
+    ],
+    '1' => [
+        'pluginName' => 'Statistics',
+        'extensionName' => 'operations',
+        'flexformFile' => 'FILE:EXT:operations/Configuration/FlexForms/flexform_statistics.xml',
+        'title' => 'Operations Statistics',
+        'icon' => 'ext-operations-wizard-icon'
+    ]
+];
 
-ExtensionUtility::registerPlugin(
-    'operations',
-    'List',
-    'Operations'
-);
-
+foreach ($pluginConfiguration as $plugin) {
+    ExtensionUtility::registerPlugin(
+        $plugin['extensionName'],
+        $plugin['pluginName'],
+        $plugin['title'],
+        $plugin['icon']
+    );
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][strtolower($plugin['extensionName']).'_'.strtolower($plugin['pluginName'])] = 'select_key';
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][strtolower($plugin['extensionName']).'_'.strtolower($plugin['pluginName'])] = 'pi_flexform';
+    ExtensionManagementUtility::addPiFlexFormValue(
+        strtolower($plugin['extensionName']).'_'.strtolower($plugin['pluginName']),
+        $plugin['flexformFile']);
+}
 
 ExtensionManagementUtility::addToInsertRecords('tx_operations_domain_model_operation');
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-
-ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:operations/Configuration/FlexForms/flexform_list.xml');
-
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'operations',
-    'Statistics',
-    'Operations Statistics'
-);
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['operations_statistics'] = 'select_key';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['operations_statistics'] = 'pi_flexform';
-
-ExtensionManagementUtility::addPiFlexFormValue('operations_statistics', 'FILE:EXT:operations/Configuration/FlexForms/flexform_statistics.xml');
