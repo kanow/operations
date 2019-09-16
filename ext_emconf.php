3c2fe14a750236aab324f7cf8fdd72b1b803dff2
Karsten Nowak <captnnowi@gmx.de>
2019-09-16 19:53:27 +0200

    [TASK] Set dependency to typo3 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'author_company' => 'undkonsorten',
 	'constraints' => array (
 		'depends' => array (
-			'typo3' => '9.5.0 - 9.5.99',
+			'typo3' => '10.1.0',
 		),
         'conflicts' => array (
         ),
