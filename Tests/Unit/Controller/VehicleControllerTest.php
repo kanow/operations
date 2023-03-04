d1f1caf47fb5b0b3cbbd790c580044772ece10c5
Karsten Nowak <captnnowi@gmx.de>
2023-03-04 17:00:24 +0100

    [TASK] Start refactoring vehicle controller test

--- Tests/Unit/Controller/VehicleControllerTest.php
+++ Tests/Unit/Controller/VehicleControllerTest.php
@@ -1,10 +1,23 @@
 <?php
 namespace Kanow\Operations\Tests;
+use Kanow\Operations\Controller\VehicleController;
+use Kanow\Operations\Domain\Model\Vehicle;
+use Kanow\Operations\Domain\Repository\VehicleRepository;
+use PHPUnit\Framework\MockObject\MockObject;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Prophecy\Prophecy\ObjectProphecy;
+use Prophecy\Prophecy\ProphecySubjectInterface;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
+use TYPO3\CMS\Fluid\View\TemplateView;
+use TYPO3\TestingFramework\Core\AccessibleObjectInterface;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
 /***************************************************************
  *  Copyright notice
  *
  *  (c) 2013 Karsten Nowak <captnnowi@gmx.de>
- *  			
+ *
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -25,7 +38,7 @@ namespace Kanow\Operations\Tests;
  ***************************************************************/
 
 /**
- * Test case for class Tx_Operations_Controller_VehicleController.
+ * Test case for class Kanow\Operations\Controller\VehicleController.
  *
  * @version $Id$
  * @copyright Copyright belongs to the respective authors
@@ -36,25 +49,70 @@ namespace Kanow\Operations\Tests;
  *
  * @author Karsten Nowak <captnnowi@gmx.de>
  */
-class VehicleControllerTest extends \TYPO3\CMS\Extbase\Tests\Unit\BaseTestCase {
-	/**
-	 * @var 
-	 */
-	protected $fixture;
+class VehicleControllerTest extends UnitTestCase {
 
-	public function setUp() {
-		$this->fixture = new \Kanow\Operations\Domain\Model\Vehicle();
-	}
+    use ProphecyTrait;
 
-	public function tearDown() {
-		unset($this->fixture);
-	}
+    /**
+     * @var VehicleController&MockObject&AccessibleObjectInterface
+     */
+    private $mockedVehicle;
 
-	/**
-	 * @test
-	 */
-	public function dummyMethod() {
-		$this->markTestIncomplete();
-	}
+    /**
+     * @var ObjectProphecy<TemplateView>
+     */
+    private $viewProphecy;
+
+    /**
+     * @var ObjectProphecy<VehicleRepository>
+     */
+    private $vehicleRepositoryProphecy;
+
+    protected function setUp(): void {
+        parent::setUp();
+
+        $this->mockedVehicle = $this->getAccessibleMock(VehicleController::class,['forward', 'redirect', 'redirectToUri']);
+
+        $this->viewProphecy = $this->prophesize(TemplateView::class);
+        $view = $this->viewProphecy->reveal();
+        $this->mockedVehicle->_set('view', $view);
+
+        $this->vehicleRepositoryProphecy = $this->prophesize(VehicleRepository::class);
+        /** @var VehicleRepository&ProphecySubjectInterface $vehicleRepository */
+        $vehicleRepository = $this->vehicleRepositoryProphecy->reveal();
+        $this->mockedVehicle->injectVehicleRepository($vehicleRepository);
+    
+    }
+
+    /**
+     * @test
+     */
+    public function isActionController(): void
+    {
+        self::assertInstanceOf(ActionController::class, $this->mockedVehicle);
+    }
+
+    /**
+     * @test
+     */
+    public function listActionAssignsAllVehicleAsVehiclesToView(): void
+    {
+        $vehicles = $this->prophesize(QueryResultInterface::class)->reveal();
+        $this->vehicleRepositoryProphecy->findAll()->willReturn($vehicles);
+        $this->viewProphecy->assign('vehicles', $vehicles)->shouldBeCalled();
+
+        $this->mockedVehicle->listAction();
+    }
+
+    /**
+     * @test
+     */
+    public function showActionAssignsPassedVehicleToView(): void
+    {
+        $vehicle = new Vehicle();
+        $this->viewProphecy->assign('vehicle', $vehicle)->shouldBeCalled();
+
+        $this->mockedVehicle->showAction($vehicle);
+    }
 
 }
