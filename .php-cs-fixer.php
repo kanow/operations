60b4fc7c8e527f473a290306ad28eb1797828f8a
Karsten Nowak <captnnowi@gmx.de>
2024-09-06 17:28:48 +0200

    [TASK] Add php cs fixer check

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,6 +4,7 @@ use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 use TYPO3\CodingStandards\CsFixerConfig;
 
 $config = CsFixerConfig::create();
+// @TODO 4.0 no need to call this manually
 $config->setParallelConfig(ParallelConfigFactory::detect());
 $config->getFinder()->in('Classes')->in('Configuration')->in('Tests');
 return $config;
